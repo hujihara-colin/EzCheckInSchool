3594bc2aaa53e2de043afdf9ab507687a29c2cc4
chillsoul <3213189@qq.com>
2020-09-11 08:47:51 +0800

    update

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ sckey = input()
 
 # 时间判断
 now = time.localtime().tm_hour + 8
-if (now >= 6) & (now <= 8):
+if (now >= 6) & (now < 8):
     templateid = "clockSign1"
     customerAppTypeRuleId = 146
-elif (now >= 12) & (now <= 14):
+elif (now >= 12) & (now < 14):
     templateid = "clockSign2"
     customerAppTypeRuleId = 147
-elif (now>=21) & (now<=22):
+elif (now >= 21) & (now< 22):
     templateid = "clockSign3"
     customerAppTypeRuleId = 148
 else:
-    print("现在是%d点，打卡时间将自动打卡" %(now))
+    print("现在是%d点%d分，打卡时间将自动打卡" %(now,time.localtime().tm_min))
     exit(0)
 
 # 随机温度(36.2~36.8)
