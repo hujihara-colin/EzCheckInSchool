96c399e43fced2f400663905f3312d4bb8a100d2
chillsoul <3213189@qq.com>
2020-09-11 08:45:41 +0800

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ userid = input()
 sckey = input()
 
 # 时间判断
-now = time.localtime().tm_hour
+now = time.localtime().tm_hour + 8
 if (now >= 6) & (now <= 8):
     templateid = "clockSign1"
     customerAppTypeRuleId = 146
